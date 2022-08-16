3703046562b723ae2f1ecbe644ee74d73a7a9e17
otalorajuand <otalorajuand@gmail.com>
2022-08-16 21:03:00 +0000

    correction of env

--- check_stat.c
+++ check_stat.c
@@ -13,12 +13,12 @@ int check_stat(char **arguments)
 
 	if (stat(arguments[0], &st) != 0)
 	{
-		if (path == NULL)
+		if (path == NULL || strcmp(arguments[0], "env") != 0)
 			res = 1;
 		if (strcmp(arguments[0], "env") != 0 && path == NULL)
 			write(STDOUT_FILENO, "/shell: No such file or directory\n", 35);
 	}
-	free(path); 
+	free(path);
 	return (res);
 }
 
